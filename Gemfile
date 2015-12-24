8e24a08bdef457d59ab5ff01bc263430f0e3ff5e
ballauriena <llc@foraker.com>
2015-12-24 10:46:11 -0700

    Setup commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
+source 'http://rubygems.org'
+
+gem "middleman", "3.3.7"
+gem "middleman-blog"
+gem 'middleman-bitballoon'
+gem 'middleman-search_engine_sitemap'
+gem 'middleman-imageoptim'
+gem "sass-json-vars"
+gem "jquery-middleman"
+gem 'jquery-ui-middleman'
+
+gem 'bitters'
+gem 'bourbon'
+gem 'neat'
+gem 'font-awesome-middleman'
+
+group :development do
+  gem "middleman-livereload", github: "middleman/middleman-livereload"
+end
---
+++ config.rb
@@ -0,0 +1,43 @@
+set :url_root, 'http://charliecrowe.bitballoon.com'
+
+# Configure assets directories
+# ----------------------------------------------
+config[:css_dir]    = 'assets/stylesheets'
+config[:js_dir]     = 'assets/javascripts'
+config[:images_dir] = 'assets/images'
+config[:fonts_dir]  = 'assets/fonts'
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+
+# Build-specific configuration
+# ----------------------------------------------
+configure :build do
+  # Use relative URLs
+  activate :directory_indexes
+
+  # Activate gzip
+  activate :gzip
+
+  # Minify CSS on build
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Add asset fingerprinting to avoid cache issues
+  activate :asset_hash, exts: %w(js css)
+
+  # Enable cache buster
+  activate :cache_buster
+
+  # Compress PNGs after build (First: gem install middleman-smusher)
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # create sitemap.xml
+  activate :search_engine_sitemap
+end
